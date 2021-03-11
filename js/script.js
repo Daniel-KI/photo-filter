bc08685fd0ca5df6c320b18278cfbab25e9c51c6
Daniil Korshov <dankorshov@gmail.com>
2021-03-11 21:51:15 +0300

    fix: fix save function

--- js/script.js
+++ js/script.js
@@ -28,6 +28,7 @@ nextImgBtn.addEventListener('click', () => {
         '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg',
         '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg',
         '19.jpg', '20.jpg'];
+    const pathToImg = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
 
     let currentImgNumber = imgNamesArray.indexOf(resultImg.src.split('/').pop());
     let newImgName = (currentImgNumber >= 0 && currentImgNumber < imgNamesArray.length - 1) ?
@@ -47,12 +48,10 @@ nextImgBtn.addEventListener('click', () => {
         timeOfDay = 'evening';
     }
 
-    resultImg.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${newImgName}`;
+    resultImg.src = `${pathToImg}${timeOfDay}/${newImgName}`;
     localStorage.setItem('loadedImg', resultImg.src);
     localStorage.setItem('loadedImgName', newImgName);
 });
-
-
 
 resetBtn.addEventListener('click', () => {
     effectSliders.forEach(slider => {
@@ -65,35 +64,35 @@ resetBtn.addEventListener('click', () => {
 
 saveBtn.addEventListener('click', () => {
     let img = new Image();
+    img.setAttribute('crossOrigin', 'anonymous'); 
     img.src = resultImg.src;
-
-    let canvas = document.createElement('canvas');
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    let ctx = canvas.getContext('2d');
-    ctx.filter = resultImg.style.filter;
+    const canvas = document.createElement('canvas');
 
     img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext('2d');
+        ctx.filter = resultImg.style.filter;
         ctx.drawImage(img, 0, 0);
+
         let link = document.createElement('a');
-        link.setAttribute('download', localStorage.getItem('loadedImgName'));
-        link.setAttribute('href', canvas.toDataURL("image/png"));
+        link.download = localStorage.getItem('loadedImgName');
+        link.href = canvas.toDataURL("image/png");
         link.click();
     };
 });
 
 fileInput.addEventListener('change', () => {
     const reader = new FileReader();
-    reader.addEventListener('load', () => {
-        let fileName = fileInput.files[0].name.split('.')[0];
-        localStorage.setItem('loadedImg', reader.result);
-        localStorage.setItem('loadedImgName', fileName);
-    });
-    reader.readAsDataURL(fileInput.files[0]);
+    const imgFile = fileInput.files[0];
+    let imgName = imgFile.name.split('.')[0];
+
     reader.onload = () => {
+        localStorage.setItem('loadedImg', reader.result);
+        localStorage.setItem('loadedImgName', imgName);
         resultImg.setAttribute('src', localStorage.getItem('loadedImg'));
-    }
+    };
+    reader.readAsDataURL(imgFile);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -108,7 +107,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 effectSliders.forEach(slider => {
-    slider.children[1].children[0].textContent = slider.children[0].children[0].value;
     slider.oninput = function () {
         slider.children[1].children[0].textContent = slider.children[0].children[0].value;
         updateEffects(slider.children[0].children[0]);
@@ -117,17 +115,15 @@ effectSliders.forEach(slider => {
 
 function updateEffects(input) {
     if (!resultImg.style.filter.includes(input.getAttribute('name'))) {
-        resultImg.style.filter = resultImg.style.filter + " " + input.getAttribute('name') + "(" + input.value + input.getAttribute('data-sizing') + ")";
+        resultImg.style.filter = `${resultImg.style.filter} ${input.getAttribute('name')}(${input.value}${input.getAttribute('data-sizing')})`;
     }
     else {
         let imgFilters = resultImg.style.filter.split(' ');
         for (let i = 0; i < imgFilters.length; i++) {
             if (imgFilters[i].includes(input.getAttribute('name')) == true) {
-                imgFilters[i] = input.getAttribute('name') + "(" + input.value + input.getAttribute('data-sizing') + ")";
+                imgFilters[i] = `${input.getAttribute('name')}(${input.value}${input.getAttribute('data-sizing')})`;
             }
         }
         resultImg.style.filter = imgFilters.join(' ');
     }
 };
-
-
